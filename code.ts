a6ad968385d4925de87ff5ee4aacb7462bdb469f
Adam Szakal <adam.b.szakal@gmail.com>
2020-03-01 22:29:41 +0100

    add support for multiple objects. fix some baggs

--- code.ts
+++ code.ts
@@ -1,23 +1,42 @@
-//
 // get selected node
-const node = figma.currentPage.selection[0];
+const selection = figma.currentPage.selection;
+const to_request = [];
 
-// get r, g, b values -> convert to hex -> concat to single string
-const r_val = Math.floor(node.fills[0].color.r * 255);
-const g_val = Math.floor(node.fills[0].color.g * 255);
-const b_val = Math.floor(node.fills[0].color.b * 255);
-const hex_val = r_val.toString(16) + g_val.toString(16) + b_val.toString(16);
+function toHex(decimal_val) {
+  let base256_val = Math.round(decimal_val * 255);
+  let hex_val = base256_val.toString(16);
+  if (base256_val < 11) {
+    return '0' + hex_val;
+  } else {
+    return hex_val;
+  }
+}
+
+for (let [key, value] of Object.entries(selection)) {
+  let node = selection[key];
+
+  // get r, g, b values from fill -> convert to hex -> concat to single string
+  const r_val = toHex(node.fills[0].color.r);
+  const g_val = toHex(node.fills[0].color.g);
+  const b_val = toHex(node.fills[0].color.b);
+  to_request.push(r_val + g_val + b_val);
+}
 
 // run ui.html and send it the hex code for the requested color
 figma.showUI(__html__, {visible: false});
-figma.ui.postMessage(hex_val);
+figma.ui.postMessage(to_request.join(','));
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
   //set nodes layer name to color name recieved through msg
-  node.name = msg;
-  //TODO: show toaster, by sending a message of a certain type
+  // node.name = msg;
+  for (let [key, value] of Object.entries(selection)) {
+    let node = selection[key];
+    node.name =
+      msg.colors[key].name + ' / ' + msg.colors[key].requestedHex.toUpperCase();
+  }
+  figma.notify('🎨 Updated names for selected layers');
   figma.closePlugin();
 };
